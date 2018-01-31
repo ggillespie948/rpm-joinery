f6bb5fa626e3809c544b6ac6ec3e5e1d7e6e64e7
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2018-01-31 21:44:40 +0000

    Add project files.

---
+++ RPMJoinery/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace RPMJoinery
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/vendor/jquery/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/vendor/bootstrap/js/bootstrap.bundle.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/vendor/bootstrap/css/bootstrap.css",
+                      "~/css/site.css"));
+        }
+    }
+}
---
+++ RPMJoinery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RPMJoinery.MvcApplication" Language="C#" %>
