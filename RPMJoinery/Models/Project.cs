353459486fc4faea071b6130c74253ebc61395f1
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2018-02-03 15:43:53 +0000

    added project class

---
+++ RPMJoinery/Models/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RPMJoinery.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public Guid UserID { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string Details { get; set; }
+
+
+    }
+}
