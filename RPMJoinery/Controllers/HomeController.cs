197204b9df3be4ae5a7df59c105d4f0dbc8a7ade
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2018-02-13 15:53:12 +0000

    Added nav link active scripts to all pages, fixed log out

--- RPMJoinery/Controllers/HomeController.cs
+++ RPMJoinery/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace RPMJoinery.Controllers
 {
@@ -55,5 +56,7 @@ namespace RPMJoinery.Controllers
 
             return View();
         }
+
+        
     }
 }
