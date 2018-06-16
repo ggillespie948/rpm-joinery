0ea40f6220dd4c31ce54b504c22c618f53bc5301
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2018-06-16 15:08:40 +0100

    -

--- RPMJoinery/Controllers/ProjectsController.cs
+++ RPMJoinery/Controllers/ProjectsController.cs
@@ -95,6 +95,10 @@ namespace RPMJoinery.Controllers
             if (ModelState.IsValid)
             {
                 project.ImgFilePath = "";
+                project.ImgFilePath2 = "";
+                project.ImgFilePath3 = "";
+                project.ImgFilePath4 = "";
+                project.ImgFilePath5 = "";
 
                 if (image != null)
                 {
@@ -120,7 +124,7 @@ namespace RPMJoinery.Controllers
                 {
                     //Save image to file
                     Stream st = image2.InputStream;
-                    string filename2 = db.Projects.ToList().Count() + "-1";
+                    string filename2 = db.Projects.ToList().Count() + "-2";
                     string bucketName2 = "rpmjoinery";
                     string s3DirectoryName2 = "";
                     string s3FileName2 = "" + filename2;
@@ -135,6 +139,66 @@ namespace RPMJoinery.Controllers
                     else
                         Response.Write("Error");
                 }
+
+                //if (image3 != null)
+                //{
+                //    //Save image to file
+                //    Stream st = image3.InputStream;
+                //    string filename = db.Projects.ToList().Count() + "-3";
+                //    string bucketName = "rpmjoinery";
+                //    string s3DirectoryName = "";
+                //    string s3FileName = "" + filename;
+                //    project.ImgFilePath3 = "https://s3-eu-west-1.amazonaws.com/rpmjoinery/" + s3FileName;
+                //    bool a2;
+                //    AmazonUploader myUploader3 = new AmazonUploader();
+                //    a2 = myUploader3.sendMyFileToS3(st, bucketName, s3DirectoryName, s3FileName);
+                //    if (a2 == true)
+                //    {
+                //        Response.Write("successfully uploaded");
+                //    }
+                //    else
+                //        Response.Write("Error");
+                //}
+
+                //if (image4 != null)
+                //{
+                //    //Save image to file
+                //    Stream st = image4.InputStream;
+                //    string filename = db.Projects.ToList().Count() + "-4";
+                //    string bucketName = "rpmjoinery";
+                //    string s3DirectoryName = "";
+                //    string s3FileName = "" + filename;
+                //    project.ImgFilePath4 = "https://s3-eu-west-1.amazonaws.com/rpmjoinery/" + s3FileName;
+                //    bool a2;
+                //    AmazonUploader myUploader4 = new AmazonUploader();
+                //    a2 = myUploader4.sendMyFileToS3(st, bucketName, s3DirectoryName, s3FileName);
+                //    if (a2 == true)
+                //    {
+                //        Response.Write("successfully uploaded");
+                //    }
+                //    else
+                //        Response.Write("Error");
+                //}
+
+                //if (image5 != null)
+                //{
+                //    //Save image to file
+                //    Stream st = image5.InputStream;
+                //    string filename = db.Projects.ToList().Count() + "-5";
+                //    string bucketName = "rpmjoinery";
+                //    string s3DirectoryName = "";
+                //    string s3FileName = "" + filename;
+                //    project.ImgFilePath5 = "https://s3-eu-west-1.amazonaws.com/rpmjoinery/" + s3FileName;
+                //    bool a2;
+                //    AmazonUploader myUploader5 = new AmazonUploader();
+                //    a2 = myUploader5.sendMyFileToS3(st, bucketName, s3DirectoryName, s3FileName);
+                //    if (a2 == true)
+                //    {
+                //        Response.Write("successfully uploaded");
+                //    }
+                //    else
+                //        Response.Write("Error");
+                //}
 
                 string tagString = "";
                 foreach(string tag in type)
