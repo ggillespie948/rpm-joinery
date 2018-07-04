41c3e65a75a2f92240777b830fb5af4018bdc571
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2018-07-04 16:52:17 +0100

    Added image descriptions to detail view, fixed image modal problem not displaying all captions, added href link to image of recent projects on index page

--- RPMJoinery/js/ImageModal.js
+++ RPMJoinery/js/ImageModal.js
@@ -1,15 +1,67 @@
-﻿// Get the modal
-var modal = document.getElementById('myModal');
+﻿
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = $('.myImg');
-var modalImg = $("#img01");
-var captionText = document.getElementById("caption");
 $('.myImg').click(function () {
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = $('.myImg');
+    var modalImg = $("#img01");
+    var captionText = document.getElementById("caption");
+
     modal.style.display = "block";
     var newSrc = this.src;
+    var cap = this.alt;
     modalImg.attr('src', newSrc);
-    captionText.innerHTML = this.alt;
+    captionText.innerHTML = cap;
+});
+
+$('.myImg2').click(function () {
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = $('.myImg2');
+    var modalImg = $("#img01");
+    var captionText = document.getElementById("caption");
+
+    modal.style.display = "block";
+    var newSrc = this.src;
+    var cap = this.alt;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = cap;
+});
+
+$('.myImg3').click(function () {
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = $('.myImg3');
+    var modalImg = $("#img01");
+    var captionText = document.getElementById("caption");
+
+    modal.style.display = "block";
+    var newSrc = this.src;
+    var cap = this.alt;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = cap;
+});
+
+$('.myImg4').click(function () {
+    // Get the modal
+    var modal = document.getElementById('myModal');
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = $('.myImg4');
+    var modalImg = $("#img01");
+    var captionText = document.getElementById("caption");
+
+    modal.style.display = "block";
+    var newSrc = this.src;
+    var cap = this.alt;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = cap;
 });
 
 // Get the <span> element that closes the modal
