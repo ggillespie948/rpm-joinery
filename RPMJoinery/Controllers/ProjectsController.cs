4523978d417eeec7072e8c50bc930869acdae619
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2018-07-09 09:13:46 +0100

    project testing

--- RPMJoinery/Controllers/ProjectsController.cs
+++ RPMJoinery/Controllers/ProjectsController.cs
@@ -350,12 +350,12 @@ namespace RPMJoinery.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,UserID,Title,Description,Type,Details,ImgDescription,ImgDescription2,ImgDescription3,ImgDescription4,ImgDescription5")] Project project)
         {
-            if (ModelState.IsValid)
-            {
-                db.Entry(project).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+            //if (ModelState.IsValid)
+            //{
+            //    db.Entry(project).State = EntityState.Modified;
+            //    db.SaveChanges();
+            //    return RedirectToAction("Index");
+            //}
             return View(project);
         }
 
