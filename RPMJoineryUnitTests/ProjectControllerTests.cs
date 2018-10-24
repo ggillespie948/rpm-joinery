1e84928301269e45d8c7a2241b6bce5afcaca655
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2018-10-24 17:25:02 +0100

    Added MOQ framework for unit testing

---
+++ RPMJoineryUnitTests/ProjectControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RPMJoinery;
+using RPMJoinery.Controllers;
+
+namespace RPMJoineryUnitTests
+{
+    [TestClass]
+    public class ProjectControllerTests
+    {
+        [TestMethod]
+        public void TestMethodIndex()
+        {
+
+            //Arrange
+            var fakeHttpContext = new Mock<HttpContextBase>();
+            var fakeIdentity = new GenericIdentity("User");
+            var principal = new GenericPrincipal(fakeIdentity, null);
+
+            fakeHttpContext.Setup(t => t.User).Returns(principal);
+            var controllerContext = new Mock<ControllerContext>();
+            controllerContext.Setup(t => t.HttpContext).Returns(fakeHttpContext.Object);
+
+            ProjectsController controller = new ProjectsController();
+
+                //Set ControllerContext with fake context
+            controller.ControllerContext = controllerContext.Object;
+
+
+            //Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ RPMJoineryUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RPMJoineryUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RPMJoineryUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cd8b1313-ec5c-4ea2-92b3-58ee71e3bb16")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
